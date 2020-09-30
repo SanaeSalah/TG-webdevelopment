0ffd6399b2c48a949669f5963d7c387f98703167
Sanae S <S.salahr@hotmail.com>
2020-09-30 10:50:29 +0200

    Javascript Intro+ Conditions

---
+++ Javascript/1. Datatypes/Script.js
@@ -0,0 +1,132 @@
+// opdracht 1
+let getal1 = null;
+console.log("getal1" + getal1);
+
+// opdracht 2
+const getal2 = null;
+console.log("getal2" + getal2);
+
+// const getal2 = 1;
+// console.log("getal2" + getal2);
+
+// opdracht 3
+// let getal3 = 3;
+// console.log("getal3" + getal3);
+
+// opdracht 4
+let getal3 = 5;
+console.log("getal3" + getal3);
+
+// opdracht 5
+// let datatype = true;
+// console.log("datatype" + typeof datatype);
+
+// opdracht 6
+// String
+// let datatype = "Sanae Salah";
+// console.log("datatype" + typeof datatype);
+
+// Number
+// let datatype = 1;
+// console.log("datatype" + typeof datatype);
+
+// BigInt
+// let datatype = BigInt(Number.MAX_SAFE_INTEGER);
+// console.log("datatype" + typeof datatype);
+
+// Symbol
+let datatype = Symbol(2);
+console.log("datatype" + typeof datatype);
+
+// opdracht 7
+let number6 = 6;
+let number8 = 8;
+console.log(number6 + number8);
+console.log(number8 - number6)
+console.log(number6 * number8);
+console.log(number8 / number6);
+
+// opdracht 8
+console.log(3 + 5 * 10 / 2);
+
+// opdracht 9
+// let number = 3;
+// number++;
+// console.log(number)
+
+// let number = 3;
+// number--;
+// console.log(number)
+
+// let number = 3;
+// const sum = number++ + 2;
+// console.log(sum);
+
+let number = 3;
+const sum = ++number + 2;
+console.log(sum);
+
+// opdracht 10
+// let number = 3;
+// number += 4;
+// console.log(number);
+// console.log(4 + 3);
+
+// opdracht 11
+// let number = 3;
+// number += 4;
+// number -= 2;
+// number /= 5;
+// number *= 4;
+// console.log(number);
+
+// opdracht 12
+let firstname = "Sanae";
+let lastname = "Salah";
+console.log(firstname + lastname)
+
+// opdracht 13
+// String
+let string = "3";
+// Number
+// let number = (3);
+let combinedText = (string + number);
+console.log(combinedText);
+
+// opdracht 14
+const webAwareAI = `<div class="cloud">
+<h1>Loading consciousness... It's loading...</h1>
+</div>`
+console.log(webAwareAI)
+
+// opdracht 15
+let text = "text";
+text += "another text";
+// console.log(text);
+console.log("text" + "another text")
+
+// opdracht 16
+// charArt
+let str = "Javascript";
+res = str.charAt(8)
+console.log(res)
+
+// concat
+let str1 = "Ik ben ";
+str2 = "Sanae Salah!";
+console.log(str1 + str2)
+
+// slice
+let fruitsoorten = ["Banaan", "Granaatappel", "Lychee", "kersen", "Mango"];
+citrus = fruitsoorten.slice(2 - 4);
+console.log(citrus);
+
+// split
+// let str = "SanaeLSalah";
+// res = str.split("L");
+// console.log(res);
+
+// Substr
+let string1 = "boeklezen";
+res = string1.substr(4, 8);
+console.log(res);
